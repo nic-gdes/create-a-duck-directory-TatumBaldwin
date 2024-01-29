e47019d445e03f1767148de73e77322bd0fee4e1
Tatum Baldwin <tjbaldwin@students.nic.edu>
2024-01-29 15:46:41 -0800

    Create files

--- index.php
+++ index.php
@@ -1,11 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+/******** CONNECT TO A DATABASE *********/
+// use mysqli_connect to connect to database. There are four arguments: host, username, password, and database name, in that order.
+
+// $conn = mysqli_connect("db:3306", "db", "db", "db" );
+
+// check if there's no connection and give an error if so
+
+// if (mysqli_connect_errno()) {
+//     echo "Connection error: " . mysqli_connect_error();
+//     exit();
+// }
+
+/******** EXECUTE QUERIES ON THE DATABASE TO RETRIEVE DATA *********/
+// write query for all records
+
+// $sql = "SELECT * FROM example";
+
+// make query and get result
+
+// $result = mysqli_query($conn, $sql);
+
+// fetch the resulting rows as an array
+
+// $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+/******** WRAP UP DATABASE CONNECTION *********/
+// free the result from memory
+
+// mysqli_free_result($result);
+
+// close the connection to the database
+
+// mysqli_close($conn);
+
+
+// print_r($data);  ----- tests that all data is there 
+
+// ?>
+
+<!-- <h1>Hello there</h1> -->
+
+<!-- <ul> -->
+    <!-- <?php foreach($data as $name) : ?> -->
+        <!-- <li> -->
+            <?php echo $name['first_name']; ?>
+        <!-- </li> -->
+        <?php endforeach ?>
+</ul>
