559157df29c0414f8bf12d09dc3901b1b724f840
Tatum Baldwin <tjbaldwin@students.nic.edu>
2024-01-30 10:26:31 -0800

    Creating bare bones for other two pages

--- create-duck.php
+++ create-duck.php
@@ -10,6 +10,24 @@
         <!-- Link to nav.php -->
         <?php include('./assets/components/nav.php'); ?>
 
+
+        <!-- Duck layout -->
+
+        <section>
+            <h2>Create a Duck</h2>
+            <form action="">
+                <label for="name">Name</label>
+                <input type="text" name="name">
+                <input type="text" name="name">
+                <input type="file" name="file">
+                <input type="text" name="name">
+                <button type="submit" name="submit" value="submit">
+                    Submit
+                </button>
+            </form>
+        </section>
+
+
         <!-- Link to footer.php -->
         <?php include('./assets/components/footer.php'); ?>
 
