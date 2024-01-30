54065463c339007a572ff2a3a1537eb02b262621
Tatum Baldwin <tjbaldwin@students.nic.edu>
2024-01-30 14:35:33 -0800

    Fix the links in nav and footer

--- assets/components/footer.php
+++ assets/components/footer.php
@@ -1,11 +1,11 @@
     <footer class="flex-column">
         <div class="logo-contain">
-            <img class="logo" src="./assets/images/transcendant_logo.png" alt="transcendant designs logo">
+            <a href="./index.php"><img class="logo" src="./assets/images/transcendant_logo.png" alt="transcendant designs logo"></a>
         </div>
 
 
-            <div><a class="footer-link" href="../index.php">Home</a></div>
-            <div><a class="footer-link" href="../contact.php">Contact</a></div>
+            <div><a class="footer-link" href="./create-duck.php">Create Ducks</a></div>
+            <div><a class="footer-link" href="./profile.php">Duck Profile</a></div>
 
         <small class="small">&copy; 2024 transcendent designs all rights reserved</small>
     </footer>
--- assets/components/nav.php
+++ assets/components/nav.php
@@ -1,9 +1,9 @@
     <header>
         <nav class="flex-row">
-        <div><a class="nav-link" href="./create-duck.php">Ducks</a></div>
+        <div><a class="nav-link" href="./create-duck.php">Create Ducks</a></div>
             <div class="logo-contain">
                 <a href="./index.php"><img class="logo" src="./assets/images/transcendant_logo.png" alt="transcendant designs logo"></a>
             </div>
-                <div><a class="nav-link" href="./profile.php">Profile</a></div>
+                <div><a class="nav-link" href="./profile.php">Duck Profile</a></div>
         </nav>
     </header>
