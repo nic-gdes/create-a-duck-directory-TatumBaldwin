a6d3094e763bd7e27ec0f7fbd1b98629bd2b3617
Tatum Baldwin <tjbaldwin@students.nic.edu>
2024-02-21 15:48:34 -0800

    Did the images

--- create-duck.php
+++ create-duck.php
@@ -1,3 +1,132 @@
+<?php 
+                
+                
+                //check for POST
+                if(isset($_POST['submit'])) {
+
+                    //Create an error array
+                    $errors = array(
+                        "name" => "",
+                        "message" => "",
+                        "food"=> "",
+                        "img_src" => ""
+                    );
+                    // check post data
+                    $name = htmlspecialchars($_POST["name"]);
+                    $food = htmlspecialchars($_POST["food"]);
+                    $message = htmlspecialchars($_POST["message"]);
+                    $img_src = $_FILES["img_src"]["name"];
+    
+                    echo $name . $food . $message;
+    
+                    // check if name exists
+                    if(empty($name)) {
+    
+                        //if it doesn't, name is required
+                        $errors['name'] = "A name is required.";
+    
+                    } else {
+                        // if it does, check against regex
+                        if(!preg_match('/^[a-z\s]+$/i', $name)) {
+                            $errors["name"] = "The name has illegal characters";
+                        } 
+                    }
+    
+                    //check if favorite food exists
+                    if(empty($food)) {
+    
+                        //if it doesn't, name is required
+                        $errors['food'] = "A Favorite Food is required.";
+    
+                    } else {
+                        // if it does, check against regex
+                        if(!preg_match('/^[a-z,\s]+$/i', $food)) {
+                            $errors["food"] = "The Favorite Food must be seperated by a comma";
+                        } 
+                    }
+    
+                    //check if bio is empty
+                    if(empty($message)) {
+                        // error if so
+                        $errors["message"] = "A Bio is required.";
+    
+                    }
+
+                    //Handle fule upload target directory
+                    $target_dir = "./assets/images/";
+
+                    //Target uploaded image file
+                    $image_file = $target_dir . basename($_FILES["img_src"]["name"]);
+
+                    // Get uploaded file extension so we can test to make sure it is an image
+                    $image_file_type = strtolower(pathinfo($image_file,PATHINFO_EXTENSION));
+
+                    // Test image for errors
+                        // images exist
+                            if(empty($img_src)) {
+                                // error if so
+                                $errors["file"] = "An image is required";
+                            } else {
+
+                                // Check that image file is an actual image
+                                $size_check = @getimagesize($_FILES["img_src"]["tmp_name"]);
+                                $file_size = $_FILES["img_src"]["size"];
+                                
+                                if(!$size_check) {
+                                    $errors["img_src"] = "File is not an image";
+                                } else if ($file_size > 500000) {
+                                    $errors["img_src"] = "Filesize cannot be larger than 500kb.";
+                                } else if ($image_file_type != "jpg" 
+                                && $image_file_type != "png"
+                                 && $image_file_type != "jpeg" 
+                                 && $image_file_type != "gif" 
+                                 && $image_file_type != "webp") {
+                                    $errors["img_src"] = "Sorry, only JPG, JPEG, PNG, GIF or WEBP files allowed";
+                                } else if (move_uploaded_file($_FILES["img_src"]["tmp_name"], $image_file)) {
+                                    //file uploaded successfully
+                                } else {
+                                    $errors["img_src"] = "Sorry, there was an error uploading your file";
+                                }
+                            }
+    
+                        if(!array_filter($errors)) {
+                            // if everything is good and form is valid
+
+                            //Connect to database
+                            require('./config/db.php');
+
+                            //build SQL query
+                            $sql = "INSERT INTO ducks (name, favorite_foods, bio, img_src) VALUES ('$name', '$food', '$message', '$image_file')";
+
+                            // echo $sql;
+                            // //Execute query in SQL
+                            mysqli_query($conn,$sql);
+                            // //load hompage
+                            echo "Query is successful. Added " . $name . " to database.";
+
+                            // header("location:./index.php");
+                        } else {
+                            // if not, run error
+                            print_r($errors);
+        
+                        }
+                
+               
+            }
+    
+                // Print results onto the page
+                // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    // $name = htmlspecialchars($_POST['name']);
+                    // $message = htmlspecialchars($_POST['message']);
+                    // $file = htmlspecialchars($_POST['file']);
+                    // $food = htmlspecialchars($_POST['food']);
+    
+                    // echo $name . ", " . $food . ", " . $file . ", " . $message;
+                // }
+            
+            
+            ?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -14,100 +143,14 @@
                 
                 <!-- PHP FOR FORM COLLECTION -->
         
-                <?php 
-                
-                
-                    //check for POST
-                    if(isset($_POST['submit'])) {
-
-                        //Create an error array
-                        $errors = array(
-                            "name" => "",
-                            "message" => "",
-                            "food"=> "",
-                        );
-                    // check post data
-                        $name = htmlspecialchars($_POST["name"]);
-                        $food = htmlspecialchars($_POST["food"]);
-                        $message = htmlspecialchars($_POST["message"]);
-        
-                        echo $name . $food . $message;
-        
-                        // check if name exists
-                        if(empty($name)) {
-        
-                            //if it doesn't, name is required
-                            $errors['name'] = "A name is required.";
-        
-                        } else {
-                            // if it does, check against regex
-                            if(!preg_match('/^[a-z\s]+$/i', $name)) {
-                                $errors["name"] = "The name has illegal characters";
-                            } 
-                        }
-        
-                        //check if favorite food exists
-                        if(empty($food)) {
-        
-                            //if it doesn't, name is required
-                            $errors['food'] = "A Favorite Food is required.";
-        
-                        } else {
-                            // if it does, check against regex
-                            if(!preg_match('/^[a-z,\s]+$/i', $food)) {
-                                $errors["food"] = "The Favorite Food must be seperated by a comma";
-                            } 
-                        }
-        
-                        //check if bio is empty
-                        if(empty($message)) {
-                            // error if so
-                            $errors["message"] = "A Bio is required.";
-        
-                        }
-        
-                        if(!array_filter($errors)) {
-                            // if everything is good and form is valid
-
-                            //Connect to database
-                            require('./config/db.php');
-
-                            //build SQL query
-                            $sql = "INSERT INTO ducks (name, favorite_foods, bio) VALUES ('$name', '$food', '$message')";
-
-                            // echo $sql;
-                            // //Execute query in SQL
-                            mysqli_query($conn,$sql);
-                            // //load hompage
-                            echo "Query is successful. Added " . $name . " to database.";
-
-                            header("location:./index.php");
-                        } else {
-                            // if not, run error
-                            print_r($errors);
-        
-                        }
-                    }
-        
-                    // Print results onto the page
-                    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                        // $name = htmlspecialchars($_POST['name']);
-                        // $message = htmlspecialchars($_POST['message']);
-                        // $file = htmlspecialchars($_POST['file']);
-                        // $food = htmlspecialchars($_POST['food']);
-        
-                        // echo $name . ", " . $food . ", " . $file . ", " . $message;
-                    // }
-                
-                
-                ?>
+              
         </div>
 
         <!-- Duck layout -->
 
         <section class="duck-container3">
             <h2>Create a Duck</h2>
-            <form action="./create-duck.php" method="POST">
+            <form action="./create-duck.php" method="POST" enctype="multipart/form-data">
                 <div class="flex-col">
                     <label for="name">Name</label>
                     <?php 
@@ -130,9 +173,14 @@
                 </div>
 
                 <div class="flex-col">
-                    <label for="profile">Profile Image</label>
+                    <label for="duck_image" >Profile Image</label>
+                    <?php if (isset($errors['img_src'])) { 
+                        echo "<div class='error'>" . $errors["img_src"] . "</div>";
+                        } 
+                        
+                        ?>
                     <div>
-                     <input type="file" name="file">
+                     <input type="file" name="img_src" accept="image/*">
                     </div>
                 </div>
 
