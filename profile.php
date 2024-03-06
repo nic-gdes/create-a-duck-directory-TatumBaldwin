d7b07611fdc2ba7c29d51a75ab3330350c86d679
Tatum Baldwin <tjbaldwin@students.nic.edu>
2024-03-06 14:50:34 -0800

    Added ducks to profile page

--- profile.php
+++ profile.php
@@ -1,3 +1,35 @@
+<?php
+
+$duck_is_live = false;
+
+//Check if the $_GET id parameter is present
+if (isset($_GET['id'])) {
+    //assign a variable to id
+    $id = htmlspecialchars($_GET['id']);
+
+// get duck info from the database
+    //connect to db - grabs info from db.php
+    require('./config/db.php');
+
+    //create query to select the intended duck from the database
+    $sql = "SELECT id, name, favorite_foods, bio, img_src FROM ducks WHERE id=$id";
+    $result = mysqli_query($conn, $sql);
+
+    //grab the one duck from the directory
+    $duck = mysqli_fetch_assoc($result);
+
+    //close connection
+    mysqli_free_result($result);
+    mysqli_close($conn);
+    
+}
+
+
+
+//Check if POST request for delete
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -8,38 +40,20 @@
 
     <body>
         <!-- Link to nav.php -->
-        <?php include('./components/nav.php'); ?>
+        <!-- <?php include('./components/nav.php'); ?> -->
 
         <!-- Duck 1 -->
         <section class="duck-container2">
-            <img src="./assets/images/coming-soon.png" alt="" class="duckimg2">
+            <img src="<?php echo $duck["img_src"]; ?>" alt="" class="duckimg2">
             <div>
-                <h2 class="duck-name">Duck Name</h2>
-                <p><span class="bolded">Favorite Foods:</span>item, item, item</p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias numquam, 
-                    soluta perferendis magnam nostrum labore in accusamus eaque architecto, 
-                    possimus natus tenetur ex rem officia deserunt hic, omnis 
-                    necessitatibus dignissimos!
-                </p>
+                <h2 class="duck-name"><?php echo $duck["name"] ?></h2>
+                <p><span class="bolded">Favorite Foods:</span><?php echo $duck["favorite_foods"] ?></p>
+                <p><?php echo $duck["bio"] ?></p>
             </div>
         </section>
 
 
-        <!-- duck 2 -->
-        <section class="duck-container2">
-            <img src="./assets/images/coming-soon.png" alt="" class="duckimg2">
-            <div>
-                <h2 class="duck-name">Duck Name</h2>
-                <p><span class="bolded">Favorite Foods:</span>item, item, item</p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias numquam, 
-                    soluta perferendis magnam nostrum labore in accusamus eaque architecto, 
-                    possimus natus tenetur ex rem officia deserunt hic, omnis 
-                    necessitatibus dignissimos!
-                </p>
-            </div>
-        </section>
+    
 
 
 
