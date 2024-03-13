018e130f4b551e041679b754f90419b07cc2ebe2
Tatum Baldwin <tjbaldwin@students.nic.edu>
2024-03-13 14:12:43 -0700

    Create index.php

---
+++ config/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Action24 Portfolio Show</title>
+    <style>
+        body {
+            background-color: #f7f7f7;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh;
+            font-family: Helvetica, Arial, sans-serif;
+        }
+        main {
+            padding: 3rem;
+            background-color: #ffffff;
+            box-shadow: 0 3px 6px rgba(0,0,0,0.15);
+        }
+        h1 {
+            color: #98002E;
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <h1>This page is under construction.</h1>
+        <p>Interested in the NIC Graphic Design program? <a href="https://nic.edu/gdes" target="_blank">View more information</a>.</p>
+    </main>
+    
+</body>
+</html>
